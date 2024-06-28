070aa4a8a8167443a93635e9d2f461b1a91d06bf
vaneath <vaneathzz@gmail.com>
2024-06-28 20:25:40 +0700

    close #49 fix conflict homepage and Fix conflict event query data

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -3,11 +3,10 @@ library home;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vodth_mobile/core/base/view_model_provider.dart';
-import 'package:vodth_mobile/core/routes/app_router.gr.dart';
 import 'package:vodth_mobile/core/routes/routes_export.dart';
 import 'package:vodth_mobile/core/theme/m3/m3_color.dart';
 import 'package:vodth_mobile/core/theme/m3/m3_text_theme.dart';
-import 'package:vodth_mobile/views/widgets/vm_tap_effect.dart';
+import 'package:vodth_mobile/views/home/local_widgets/events_list.dart';
 
 import 'home_view_model.dart';
 

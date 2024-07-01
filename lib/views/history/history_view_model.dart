8b2602f38f7ab93a1e9614a5c5248263d524f163
Vuthy Outhdom <vuthyouthdom01@gmail.com>
2024-07-01 14:03:16 +0700

    history page

--- lib/views/history/history_view_model.dart
+++ lib/views/history/history_view_model.dart
@@ -4,6 +4,9 @@ class HistoryViewModel extends BaseViewModel {
   String _selectedChip = 'All';
   String get selectedChip => _selectedChip;
 
+  int get eventCount => _eventCount;
+  int _eventCount = 0;
+
   void selectChip(String chip) {
     _selectedChip = chip;
     notifyListeners();

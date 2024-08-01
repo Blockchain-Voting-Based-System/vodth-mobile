5269629ab80b31d01868ded6c185527c7f7222ba
ouudom <vuthyouthdom.01@gmail.com>
2024-08-01 13:31:10 +0700

    authentication functionality

--- lib/views/login/login_adaptive.dart
+++ lib/views/login/login_adaptive.dart
@@ -1,151 +1,158 @@
 part of 'login_view.dart';
 
-class _LoginAdaptive extends StatelessWidget {
+class _LoginAdaptive extends StatefulWidget {
   const _LoginAdaptive(this.viewModel);
 
   final LoginViewModel viewModel;
 
-  Widget _buildEmailField() {
-    return const TextField(
-      decoration: InputDecoration(
-        labelText: 'Email',
-        border: OutlineInputBorder(),
-      ),
-      keyboardType: TextInputType.emailAddress,
-    );
-  }
+  @override
+  _LoginAdaptiveState createState() => _LoginAdaptiveState();
+}
 
-  Widget _buildPasswordField() {
-    return StatefulBuilder(
-      builder: (context, setState) {
-        bool isPasswordVisible = false;
-        return TextField(
-          obscureText: !isPasswordVisible,
-          decoration: InputDecoration(
-            labelText: 'Password',
-            border: const OutlineInputBorder(),
-            suffixIcon: IconButton(
-              icon: Icon(
-                // ignore: dead_code
-                isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-              ),
-              onPressed: () {
-                setState(() {
-                  isPasswordVisible = !isPasswordVisible;
-                });
-              },
+class _LoginAdaptiveState extends State<_LoginAdaptive> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
+  bool _isLoading = false; // Loading state
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height - 48.0,
             ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget _buildBody(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(24.0),
-      child: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: MediaQuery.of(context).size.height -
-                48.0, // Adjust based on the padding
-          ),
-          child: IntrinsicHeight(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      const TextSpan(
-                        text: 'Login to ',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(
-                        text: 'Vodth',
-                        style: TextStyle(
-                          color: M3Color.of(context).primary,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 36),
-                _buildEmailField(),
-                const SizedBox(height: 16),
-                _buildPasswordField(),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: M3Color.of(context).primary,
-                    minimumSize: const Size(double.infinity, 50),
-                  ),
-                  onPressed: () {
-                    context.router.push(const HomeRoute());
-                  },
-                  child: const Center(
-                    child: Text(
-                      'Login',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      // Handle forget password action
-                    },
-                    child: const Text('Forget password?'),
-                  ),
-                ),
-                const SizedBox(height: 36),
-                const Row(
-                  children: [
-                    Expanded(child: Divider()),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text('or Log in with'),
-                    ),
-                    Expanded(child: Divider()),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Handle Google login action
-                  },
-                  icon: Image.asset(
-                    'assets/icons/google.png',
-                    height: 24,
-                    width: 24,
-                  ),
-                  label: const Text('Google'),
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 50),
-                    side: const BorderSide(color: Colors.grey),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Row(
+            child: IntrinsicHeight(
+              child: Form(
+                key: _formKey,
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Text("Not yet have account?"),
-                    TextButton(
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'Login to ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Vodth',
+                            style: TextStyle(
+                              color: M3Color.of(context).primary,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 36),
+                    _buildEmailField(),
+                    const SizedBox(height: 16),
+                    _buildPasswordField(),
+                    const SizedBox(height: 24),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: M3Color.of(context).primary,
+                        minimumSize: const Size(double.infinity, 50),
+                      ),
+                      onPressed: _isLoading
+                          ? null
+                          : () async {
+                              if (_formKey.currentState?.validate() ?? false) {
+                                setState(() {
+                                  _isLoading = true;
+                                });
+
+                                await widget.viewModel.login(
+                                  context,
+                                  _emailController.text,
+                                  _passwordController.text,
+                                );
+
+                                setState(() {
+                                  _isLoading = false;
+                                });
+                              }
+                            },
+                      child: _isLoading
+                          ? const Center(
+                              child: SizedBox(
+                                width: 24,
+                                height: 24,
+                                child: CircularProgressIndicator(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            )
+                          : const Center(
+                              child: Text(
+                                'Login',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          // Handle forget password action
+                        },
+                        child: const Text('Forget password?'),
+                      ),
+                    ),
+                    const SizedBox(height: 36),
+                    const Row(
+                      children: [
+                        Expanded(child: Divider()),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Text('or Log in with'),
+                        ),
+                        Expanded(child: Divider()),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    ElevatedButton.icon(
                       onPressed: () {
-                        context.router.push(const RegisterRoute());
+                        // widget.viewModel.loginWithGoogle(context);
                       },
-                      child: const Text('Create account'),
+                      icon: Image.asset(
+                        'assets/icons/google.png',
+                        height: 24,
+                        width: 24,
+                      ),
+                      label: const Text('Google'),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        side: const BorderSide(color: Colors.grey),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Not yet have account?"),
+                        TextButton(
+                          onPressed: () {
+                            context.router.push(const RegisterRoute());
+                          },
+                          child: const Text('Create account'),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
@@ -153,11 +160,52 @@ class _LoginAdaptive extends StatelessWidget {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: _buildBody(context),
+  Widget _buildEmailField() {
+    return TextFormField(
+      controller: _emailController,
+      decoration: const InputDecoration(
+        labelText: 'Email',
+        border: OutlineInputBorder(),
+      ),
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter an email';
+        }
+        // Regex for validating email address
+        final emailRegExp = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+        if (!emailRegExp.hasMatch(value)) {
+          return 'Please enter a valid email address';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return TextFormField(
+      controller: _passwordController,
+      obscureText: !_isPasswordVisible,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        border: const OutlineInputBorder(),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+          ),
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter a password';
+        }
+        return null;
+      },
     );
   }
 }

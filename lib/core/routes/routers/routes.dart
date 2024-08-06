ca0177e4fe8f670944d1c2e764e0d05a1ebe4008
ouudom <vuthyouthdom.01@gmail.com>
2024-08-06 14:09:35 +0700

    update about us & faqs

--- lib/core/routes/routers/routes.dart
+++ lib/core/routes/routers/routes.dart
@@ -22,4 +22,5 @@ final routers = [
   AutoRoute(page: EventDetailRoute.page, path: '/events/:id'),
   AutoRoute(page: FaqsRoute.page),
   AutoRoute(page: CastingVoteRoute.page),
+  AutoRoute(page: AboutUsRoute.page)
 ];
